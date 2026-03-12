03f1364bc2b05c4cef91c132a147f9fd636917a0
prerit0 <masihprerit@gmail.com>
2026-03-12 17:39:44 +0530

    static page

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from app.chat_api import router as chat_router
 from app.purchase_api import router as purchase_router
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
 
 app = FastAPI(title="Gold Investment AI API")
 
 app.include_router(chat_router)
 app.include_router(purchase_router)
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+@app.get("/")
+def home():
+    return FileResponse("app/static/index.html")
