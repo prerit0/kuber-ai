921b9916238ac12647e237323206eb4ed8a3ae1c
prerit0 <masihprerit@gmail.com>
2026-03-12 17:46:44 +0530

    removed static page

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 from app.chat_api import router as chat_router
 from app.purchase_api import router as purchase_router
 
@@ -7,11 +6,3 @@ app = FastAPI(title="Gold Investment AI API")
 
 app.include_router(chat_router)
 app.include_router(purchase_router)
-
-@app.get("/", response_class=HTMLResponse)
-def home():
-    return """
-    <h1>Gold Investment AI</h1>
-    <p>API is running successfully.</p>
-    <p>Go to <a href='/docs'>API Documentation</a></p>
-    """
